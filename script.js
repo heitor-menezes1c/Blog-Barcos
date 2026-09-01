c066ea443651089d27c25396ae3bbe2a4782a424
heitor-menezes1c <heitor.menezes@escola.pr.gov.br>
2026-08-31 21:00:45 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
 function botaoClicado() {
     console.log("fui clicado");
     let texto = botao.querySelector("span");
